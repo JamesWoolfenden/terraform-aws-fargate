73b763d098c5cc1579d90ea43ece8f3e225914a0
James Woolfenden <james.woolfenden@gmail.com>
2026-05-03 20:12:23 +0100

    fix: replace deprecated aws_subnet_ids with aws_subnets

--- example/examplea/data.tf
+++ example/examplea/data.tf
@@ -1,10 +1,9 @@
 data "aws_vpcs" "cluster" {}
-
-data "aws_subnet_ids" "cluster" {
-  vpc_id = element(tolist(data.aws_vpcs.cluster.ids), 0)
+data "aws_subnets" "cluster" {
+  filter {
+    name   = "vpc-id"
+    values = [element(tolist(data.aws_vpcs.cluster.ids), 0)]
+  }
 }
-
 data "aws_caller_identity" "current" {}
-
-
 data "aws_region" "current" {}
--- example/examplea/module.fargate.tf
+++ example/examplea/module.fargate.tf
@@ -5,7 +5,7 @@ module "fargate" {
   task_definition  = var.task_definition
   service_name     = "my-first-service"
   lb               = var.lb
-  subnets          = data.aws_subnet_ids.cluster.ids
+  subnets          = data.aws_subnets.cluster.ids
   target_group_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.target_group_arn}"
   vpc_id           = element(tolist(data.aws_vpcs.cluster.ids), 0)
 }

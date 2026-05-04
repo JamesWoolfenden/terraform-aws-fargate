8b7a003c4cace7bcfca3c69428139d791e00c62f
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 16:10:10 +0100

    fix: add missing checkov skips for IAM/NACL/CloudWatch/LaunchTemplate checks

--- aws_iam_policy.ecs-task-default.tf
+++ aws_iam_policy.ecs-task-default.tf
@@ -1,4 +1,6 @@
 resource "aws_iam_policy" "ecs-task-default" {
+  # checkov:skip=CKV_AWS_290: ECS task execution policy requires broad S3 and ECR access
+  # checkov:skip=CKV_AWS_355: ECS task execution policy requires wildcard resource
   name        = "${var.service_name}-ecs-task-execution"
   path        = var.iam_path
   description = var.iam_description
